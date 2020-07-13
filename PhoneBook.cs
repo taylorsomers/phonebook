519600eae9afb812a7440bdf31fb32a75a0d3c30
Taylor Somers <tsomers@yandex.com>
2020-07-13 10:38:43 -0400

    Add logic to Main() method

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -7,7 +7,26 @@ class PhoneBook
 
   static void Main()
   {
-
+    Console.WriteLine("MAIN MENU");
+    Console.WriteLine("Would you like to add a person to your phone book? ['Y' for yes, 'Enter' for no]");
+    string answer = Console.ReadLine();
+    if (answer == "Y" || answer == "y")
+    {
+      AddContact();
+    }
+    else
+    {
+      Console.WriteLine("Would you like to look up a number in your phone book? ['Y' for yes, 'Enter' for no]");
+      string finishedAnswer = Console.ReadLine();
+      if (finishedAnswer == "Y" || finishedAnswer == "y")
+      {
+        Console.WriteLine("Goodbye.");
+      }
+      else
+      {
+        Main();
+      }
+    }
   }
 
   static void AddContact()
@@ -17,6 +36,6 @@ class PhoneBook
 
   static void LookUpContact()
   {
-    
+
   }
 }
