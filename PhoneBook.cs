00bb0efdca33c1555b4cd1613b62e81858dd5ba9
Taylor Somers <tsomers@yandex.com>
2020-07-13 10:22:59 -0400

    Set up dictionary

--- PhoneBook.cs
+++ PhoneBook.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+class PhoneBook
 {
   static void Main()
   {
-    
+    public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+    static void Main();
   }
 }
